bc35212052803ac8f36d44cb4abe46c56977a32b
Prasad <prasad29896@gmail.com>
2023-09-16 22:09:48 +0530

    tictactoe bug fixes #143

--- tictactoe.js
+++ tictactoe.js
@@ -2,7 +2,8 @@ var clickCount = 0;
 var timerValue = 10;
 var hasStarted = false;
 var interval;
-var winning = ["012", "036", "048", "345", "678", "246", "147", "258"];
+var winning = ["0,1,2", "0,3,6", "0,4,8", "3,4,5", "6,7,8", "2,4,6", "1,4,7", "2,5,8"];
+
 
 function tileClick(index) {
     if (hasStarted) {
@@ -11,16 +12,16 @@ function tileClick(index) {
         if (tiles[index].innerHTML.length == 0) {
 
             tiles[index].innerHTML = clickCount % 2 == 0 ? "X" : "O";
-            if(clickCount % 2 == 0){
+            if (clickCount % 2 == 0) {
                 tiles[index].classList.add("orange");
                 tiles[index].classList.remove("blue");
-                
-            }else {
+
+            } else {
                 tiles[index].classList.add("blue");
                 tiles[index].classList.remove("orange");
 
             }
-        
+
             clickCount = clickCount + 1;
             resetTime();
             disablePlayer();
@@ -32,7 +33,15 @@ function tileClick(index) {
                     document.getElementsByClassName("name")[0].innerHTML = "Winner !";
 
                 }
-                reset();
+                reset(false);
+            }
+            if (clickCount == 9 && !playerWin) {
+                console.log("tie");
+                document.getElementsByClassName("name")[0].innerHTML = "TIE !";
+                document.getElementsByClassName("name")[1].innerHTML = "TIE !";
+                reset(false);
+                clearInterval(interval);
+                timerValue = 10;
             }
 
         }
@@ -50,9 +59,11 @@ function startClicked() {
         resetTime();
         document.getElementById("start").innerHTML = "STOP";
         const name = Array.from(document.getElementsByClassName("name"));
+        for (let index = 0; index < 9; index++) {
+            document.getElementsByClassName("tile")[index].innerHTML = "";
+        }
         for (let index = 0; index < name.length; index++) {
             document.getElementsByClassName("name")[index].innerHTML = "";
-
         }
     } else {
         reset();
@@ -101,14 +112,17 @@ function disablePlayer() {
     }
 }
 
-function reset() {
+function reset(shouldReset = true) {
     const players = document.getElementsByClassName("player");
 
     players[1].classList.remove("disabled");
     players[0].classList.remove("disabled");
     const tiles = document.getElementsByClassName("tile");
-    for (let index = 0; index < tiles.length; index++) {
-        document.getElementsByClassName("tile")[index].innerHTML = "";
+    if (shouldReset) {
+        for (let index = 0; index < tiles.length; index++) {
+            document.getElementsByClassName("tile")[index].innerHTML = "";
+        }
+
     }
     document.getElementById("start").innerHTML = "START";
     clearInterval(interval);
@@ -121,21 +135,35 @@ function reset() {
 function checkWinning(player) {
 
     const tiles = Array.from(document.getElementsByClassName("tile"));
-    let comb = "";
+    let comb = [];
     let winningValue = false;
     tiles.forEach((element, index) => {
         if (element.innerHTML == player) {
 
-            comb += index;
+            // comb += (index != 0 ? "," : "" )+ index;
+            comb.push(index);
         }
     });
-
     winning.forEach(el => {
-        if (comb.includes(el)) {
+        // if (comb.includes(el)) {
+        //     winningValue = true;
+        // }
+        let correctCount = 0;
+
+
+        winningComb = el.split(",");
+        winningComb.forEach(ele => {
+            comb.forEach(c => {
+                if (ele == c) {
+                    correctCount += 1;
+                }
+            })
+        })
+
+        if (correctCount >= 3) {
             winningValue = true;
         }
-
+        // return winningValue;
     })
     return winningValue;
-
 }
